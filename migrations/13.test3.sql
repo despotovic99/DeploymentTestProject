48f9dba3b423484176ebbc8a3f6cc92e3c333085
despotovic <d.neca99@gmail.com>
2023-07-07 09:36:55 +0200

    added migration 13

---
+++ migrations/13.test3.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `test_tabela_tri` (  testName VARCHAR(255),  testNumber INT);
---
+++ migrations_pg/13.test3PG.sql
@@ -0,0 +1,4 @@
+CREATE TABLE "test_tabela_tri" (
+                                                         "testName" VARCHAR,
+                                                         "testNumber" INT
+);
