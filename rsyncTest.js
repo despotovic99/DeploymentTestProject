09d04c9994f8912291457c421c67d587ad6ac0e1
nemanja <d.neca99@gmail.com>
2023-07-11 22:34:06 +0200

    test rsync skripte

--- rsyncTest.js
+++ rsyncTest.js
@@ -4,7 +4,13 @@ const exec = util.promisify(require('child_process').exec);
 
     console.log('rsync started')
 
-    await exec('rsync -avz --progress -e "ssh -i ~/.ssh/nemanja " root@172.18.0.2:/test ./testDir/');
+let result =     await exec('rsync -avz --progress -e "ssh -i ~/.ssh/nemanja " root@172.18.0.2:/test ./testDir/');
+console.log(result.stdout)
 
+ result =     await exec('ls testDir');
+console.log(result.stdout)
+
+ result =     await exec('node ./testDir/test/test.js');
+console.log(result.stdout)
 
 })()
